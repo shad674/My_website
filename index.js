98869fd31a06bd39eea006eb3c54e5e34cb167f5
shad674 <shad.reyes25@gmail.com>
2021-07-24 16:15:12 -0700

    add javascript to change the color

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 function imgChanger(anything){
     document.querySelector('.shadImg').src = anything;
 }
+
+function colorChanger(color){
+    const bg = document.querySelectorAll('.bg');
+    const bgc = document.querySelectorAll('.color')
+    for (var i = 0; i < bg.length; i++){
+        bg[i].style.background = color;
+        bgc[i].style.color = color;
+    }
+
+  
+    
+}
